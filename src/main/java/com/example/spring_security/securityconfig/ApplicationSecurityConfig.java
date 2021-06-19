21359a96c1ab7ebbda414201347dffc4e5747197
tejas181995 <tejasdev95@gmail.com>
2021-06-19 23:18:32 +0530

    [Tejas]: Added student as user role and authorized it

--- src/main/java/com/example/spring_security/securityconfig/ApplicationSecurityConfig.java
+++ src/main/java/com/example/spring_security/securityconfig/ApplicationSecurityConfig.java
@@ -1,21 +1,54 @@
 package com.example.spring_security.securityconfig;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import static com.example.spring_security.securityconfig.ApplicationUserRole.*;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/", "index").permitAll()
+                .antMatchers("/api/**").hasRole(STUDENT.name())
                 .anyRequest()
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails atharvaUser = User.builder()
+                .username("atharva")
+                .password(passwordEncoder.encode("password"))
+                .roles(STUDENT.name()) //role student
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                atharvaUser
+        );
     }
 }
---
+++ src/main/java/com/example/spring_security/securityconfig/ApplicationUserRole.java
@@ -0,0 +1,22 @@
+package com.example.spring_security.securityconfig;
+
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
+import static com.example.spring_security.securityconfig.ApplicationUserPermission.*;
+
+public enum ApplicationUserRole {
+    STUDENT(Sets.newHashSet()),
+    ADMIN(Sets.newHashSet(COURSE_READ, COURSE_WRITE, STUDENT_READ, STUDENT_WRITE));
+
+    private final Set<ApplicationUserPermission> permissions;
+
+    ApplicationUserRole(Set<ApplicationUserPermission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public Set<ApplicationUserPermission> getPermissions() {
+        return permissions;
+    }
+}
